edcb9541768ba7c39fefb657f1c1ce09364416c7
Bellamkonda Deepak <65393385+deepak-bellamkonda@users.noreply.github.com>
2024-01-03 00:17:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,9 +31,17 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--disable-notifications')
+chrome_options.add_argument('--no-sandbox') 
 def setup_driver():
     global driver
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.execute_cdp_cmd('Network.setUserAgentOverride', {
