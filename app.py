9a9df838ebc0f5f77620ea10d7175fc3ac91dcc3
Bellamkonda Deepak <65393385+deepak-bellamkonda@users.noreply.github.com>
2024-01-03 16:50:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ import chromedriver_autoinstaller
 
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY'] 
 System.setProperty("webdriver.chrome.driver", webDriverExec)
-ChromeOptions  = new ChromeOptions()
+ChromeOptions  = ChromeOptions()
 chromeOptions.addArguments("--headless");
 chromeOptions.add_argument("--disable-notifications")
 chromeOptions.addArguments("window-size=1200,1100")
